c61a81ebe3b759825ea1fbcd87ca65c86e2a6956
Radoya aftab <raimaaftab73@gmail.com>
2024-09-11 21:32:15 +0600

    first commit

---
+++ lists.py
@@ -0,0 +1,17 @@
+li = ["Bombastic", "osama dustbin ladder", "911 special"]
+
+print(li)
+print(li[0] )
+print(li[1] )
+print(li[2] )
+
+numbers = [1,2,3,4,5]
+print(numbers)
+print(numbers[-1])
+print(numbers[-2])
+
+alist = [100,200,300,400,500]
+print( alist[::-1])
+    
+
+
